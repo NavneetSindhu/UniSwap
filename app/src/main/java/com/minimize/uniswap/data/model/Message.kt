68676b2b9e53191e65378b32b02b8b436b9499c0
Navneet Sindhu <nsindhu1610@gmail.com>
2026-08-18 17:48:00 +0530

    Commit 1: Cleanup unused API & Update Message model

--- app/src/main/java/com/minimize/uniswap/data/model/Message.kt
+++ app/src/main/java/com/minimize/uniswap/data/model/Message.kt
@@ -1,12 +1,19 @@
 package com.minimize.uniswap.data.model
 
+import com.google.firebase.Timestamp
 import java.util.UUID
+
+enum class MessageStatus {
+    SENDING, SENT, FAILED
+}
 
 data class Message(
     val id: String = UUID.randomUUID().toString(),
-    val senderId: String="",
-    val text: String="",
+    val senderId: String = "",
+    val text: String = "",
     val timestamp: Long = System.currentTimeMillis(),
+    val firestoreTimestamp: Timestamp? = null, // Used for server-side sorting
     val isLocationPin: Boolean = false,
-    val locationName: String? = null
+    val locationName: String? = null,
+    val status: MessageStatus = MessageStatus.SENT
 )
